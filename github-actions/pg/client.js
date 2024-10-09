4b87a4691ef2b073490b0c2c8b6af826c194cf7c
ezzat223 <ezzat22hegazy@gmail.com>
2024-10-09 15:13:38 +0300

    Modify pg

--- github-actions/pg/client.js
+++ github-actions/pg/client.js
@@ -1,29 +1,42 @@
-const { Client } = require('pg');
+require('dotenv').config(); // load .env file
+const { Client } = require("pg");
 
 const pgclient = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres'
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: "postgres",
+  password: "postgres",
+  database: "postgres",
 });
 
 pgclient.connect();
 
-const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+// ------------------------ Create a Table and insert values for testing ------------------------ //
+const table =
+  "CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))";
+const text =
+  "INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *";
+const values = [
+  "Mona",
+  "Octocat",
+  9,
+  "88 Colin P Kelly Jr St, San Francisco, CA 94107, United States",
+  "octocat@github.com",
+];
 
+// create the table
 pgclient.query(table, (err, res) => {
-    if (err) throw err
+  if (err) throw err;
 });
 
+// insert values
 pgclient.query(text, values, (err, res) => {
-    if (err) throw err
+  if (err) throw err;
 });
 
-pgclient.query('SELECT * FROM student', (err, res) => {
-    if (err) throw err
-    console.log(err, res.rows) // Print the data in student table
-    pgclient.end()
+// select statement for testing
+pgclient.query("SELECT * FROM student", (err, res) => {
+  if (err) throw err;
+  console.log(err, res.rows); // Print the data in student table
+  pgclient.end();
 });
